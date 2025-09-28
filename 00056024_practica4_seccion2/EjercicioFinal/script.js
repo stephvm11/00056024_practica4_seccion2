a7086578f0501fad6d4d8a295966f26e58ef3c2e
stephvm11 <00056024@uca.edu.sv>
2025-09-27 23:39:27 -0600

    fix: Corregir formato de archivos

--- 00056024_practica4_seccion2/EjercicioFinal/script.js
+++ 00056024_practica4_seccion2/EjercicioFinal/script.js
@@ -1,30 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const selectorDia = document.getElementById('selectorDias');
-    const botonAceptar = document.getElementById('aceptarBtn');
-    const recordatorio = document.getElementById('mensaje');
+document.addEventListener("DOMContentLoaded", () => {
+  const selectorDia = document.getElementById("selectorDias");
+  const botonAceptar = document.getElementById("aceptarBtn");
+  const recordatorio = document.getElementById("mensaje");
 
-    const listaActividades = {
-        "Lunes":"Atender un cliente especifico.",
-        "Martes":"Visitar una agencia fuera de la ciudad.",
-        "Miercoles":"Llevar a mi hija al ballet (balé).",
-        "Jueves":"Priorizar entregas de desarrollo.",
-        "Viernes":"Atender problemas de manera remota.",
-        "Sabado":"Hacer lo que mi esposa quiera.",
-        "Domingo":"No tengo actividades para este día."
-    };
+  const listaActividades = {
+    Lunes: "Atender un cliente especifico.",
+    Martes: "Visitar una agencia fuera de la ciudad.",
+    Miercoles: "Llevar a mi hija al ballet (balé).",
+    Jueves: "Priorizar entregas de desarrollo.",
+    Viernes: "Atender problemas de manera remota.",
+    Sabado: "Hacer lo que mi esposa quiera.",
+    Domingo: "No tengo actividades para este día.",
+  };
 
-    function crearRecordatorio(dia, actividad){
-        return dia + ". " + actividad;
+  function crearRecordatorio(dia, actividad) {
+    return dia + ". " + actividad;
+  }
+
+  botonAceptar.addEventListener("click", () => {
+    const diaSeleccionado = selectorDia.value;
+    const actividadSeleccionada = listaActividades[diaSeleccionado];
+
+    if (diaSeleccionado) {
+      recordatorio.innerHTML = crearRecordatorio(
+        diaSeleccionado,
+        actividadSeleccionada
+      );
+    } else {
+      recordatorio.innerHTML = "Por favor, selecciona un día de la semana.";
     }
-
-    botonAceptar.addEventListener('click', () => {
-        const diaSeleccionado = selectorDia.value;
-        const actividadSeleccionada = listaActividades[diaSeleccionado];
-        
-        if (diaSeleccionado) {
-            recordatorio.innerHTML = crearRecordatorio(diaSeleccionado, actividadSeleccionada);
-        } else {
-            recordatorio.innerHTML = 'Por favor, selecciona un día de la semana.';
-        }
-    });
+  });
 });
